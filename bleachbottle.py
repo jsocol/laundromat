431629f7e326ac8ff369c36110022daee7151a43
James Socol <james@mozilla.com>
2011-10-25 14:59:09 -0400

    Echo back input in textarea.

--- bleachbottle.py
+++ bleachbottle.py
@@ -12,7 +12,7 @@ def index():
     if dirty:
         clean = jinja2.Markup(bleach.clean(dirty))
     else:
-        clean = None
+        clean = ''
 
     return render_template('index.html', clean=clean)
 
@@ -22,9 +22,11 @@ if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option('-d', '--debug', action='store_true', default=False,
                       help='Run the server in debug mode.')
+    parser.add_option('-H', '--host', action='store', default='0.0.0.0',
+                      help='The address to bind to.')
     parser.add_option('-p', '--port', action='store', default=8000, type='int',
                       help='Port to listen on.')
     opts, _ = parser.parse_args()
 
     app.debug = opts.debug
-    app.run(port=opts.port)
+    app.run(host=opts.host, port=opts.port)
